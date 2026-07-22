6387c43b9f835fef314683a307417885eebfa385
chzhxkd100 <chzhxkd100@naver.com>
2026-07-22 17:57:51 +0900

    refactor: Reset project completely

--- server/src/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import path from "path";
-
-const dbPath = path.join(__dirname, "../dev.db");
-const adapter = new PrismaBetterSqlite3({ url: "file:" + dbPath });
-
-export const prisma = new PrismaClient({ adapter });
